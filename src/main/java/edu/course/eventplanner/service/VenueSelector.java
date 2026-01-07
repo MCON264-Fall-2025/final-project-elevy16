722ab9e2a8c406ee1f36b77b98e695f1311db03c
elevy16 <elevy16@student.touro.edu>
2026-01-06 17:07:53 -0800

    Create VenueSelectorTest and implement testOneValidValue unit test

--- src/main/java/edu/course/eventplanner/service/VenueSelector.java
+++ src/main/java/edu/course/eventplanner/service/VenueSelector.java
@@ -6,5 +6,17 @@ import java.util.*;
 public class VenueSelector {
     private final List<Venue> venues;
     public VenueSelector(List<Venue> venues) { this.venues = venues; }
-    public Venue selectVenue(double budget, int guestCount) { return null; }
+
+    public Venue selectVenue(double budget, int guestCount) {
+        // The program asks the user for event budget and number of guests
+
+        // venue is valid if:
+        // Its cost is less than or equal to the budget
+        // Its capacity is greater than or equal to the number of guests
+
+        // From all valid venues, select the best venue:
+        // (Lowest cost If tied, smallest capacity that still fits
+
+
+    }
 }
--- src/test/java/edu/course/eventplanner/service/VenueSelectorTest.java
+++ src/test/java/edu/course/eventplanner/service/VenueSelectorTest.java
@@ -1,4 +1,30 @@
 package edu.course.eventplanner.service;
 
-public class venueSelectorTest {
+import edu.course.eventplanner.model.Guest;
+import edu.course.eventplanner.model.Venue;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VenueSelectorTest {
+
+    @Test
+    public void testOneValidVenue() {
+        // arrange venue and venue selector
+        Venue weddingVenue = new Venue("wedding", 10000.00, 200, 20, 10);
+        List<Venue> venues = new ArrayList<>();
+        venues.add(weddingVenue);
+        VenueSelector venueSelector = new VenueSelector(venues);
+
+        // select budget and guest count
+        Venue selected = venueSelector.selectVenue(10000.00, 100);
+
+        // assert valid venue
+        assertEquals(selected, weddingVenue);
+    }
+
 }
